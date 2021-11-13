28a15017748fdb8ebe9e6d26b6ed67a0105ec4c1
yeasinopu17 <yeasinopu@gmail.com>
2021-11-13 23:36:08 +0600

    Expenses

---
+++ README.md
@@ -0,0 +1,2 @@
+# React - The Complete Guide (incl Hooks, React Router, Redux)
+## Instructor: Maximilian Schwarzmüller
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,13 @@
 import './ExpenseItem.css';
-function ExpenseItem() {
+import ExpenseDate from './ExpenseDate';
+
+function ExpenseItem(props) {
     return (
         <div className="expense-item">
-            <div>Mar 28, 2021</div>
+            <ExpenseDate date={props.date}/>
             <div className="expense-item__description">
-                <h2>Car Insurance</h2>
-                <div className="expense-item__price">$29.99</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">$ {props.amount}</div>
             </div>
         </div>
     );
